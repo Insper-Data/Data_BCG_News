75fa3f0c2991c456192c2798e9c9d3c5af05b186
Max <59523289+mittelmax@users.noreply.github.com>
2021-03-01 19:24:41 -0300

    adicionei dicionário com infos sobre run ids antigos

--- aux_funcs/save_run_variables.py
+++ aux_funcs/save_run_variables.py
@@ -1,14 +1,25 @@
 from datetime import date
 import pandas as pd
 from Vars.var_funcs import *
-
+from os import path
+import pickle as pk
 
 def save_run_variable(path_drive, run_id, func_name):
 
     df = pd.read_csv(f"{path_drive}/{run_id}.csv")
     data = str(date.today())
-
     var = eval(func_name)(path_drive, run_id)
     df = df["unique_identifier"].join(var, how="left", on="unique_identifier")
 
-    df.to_csv(f"{path_drive}/Variables/{func_name}_{data}.csv")
+    # Pickle with preproc info
+    info = {"preproc": run_id}
+    
+
+    # Checando se arquivo já existe para determinar o run id
+    for i in range(0, 1000):
+        path_file = f'{path_drive}/Variables/{func_name}_{data}_{i}.csv'
+        if not path.exists(path_file):
+            df.to_csv(f'{path_drive}/Variables/{func_name}_{data}_{i}.csv')
+            with open(f'{path_drive}/Variables/dict_{func_name}_{data}_{i}', 'wb') as file:
+                pk.dump(info, file)
+            break

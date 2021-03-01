f0552c35e78dac6ea933f20f734dbda3294bf801
Max <59523289+mittelmax@users.noreply.github.com>
2021-03-01 18:52:05 -0300

    Arrumei run id

--- aux_funcs/save_run_prepoc.py
+++ aux_funcs/save_run_prepoc.py
@@ -1,36 +1,42 @@
 from datetime import date
 from numpy import nan
-from oficial import retorna_texto_limpo
 import pandas as pd
 from Prepoc.prepoc_funcs import *
+from os import path
 
 
-def save_run_prepoc(path_drive, drop_punct=False, strip_accents=False, drop_stopwords=False, stem_and_lem=False, clean_text=True):
-	
+def save_run_prepoc(path_drive, drop_punct=False, strip_accents=False, drop_stopwords=False, stem_and_lem=False,
+                    clean_text=True):
     df = pd.read_csv(path_drive + "/Raw/Values/index.csv")
     coluna_run_id = list(df.unique_identifier)
     lista_artigo_limpo = []
     for run_id in coluna_run_id:
-        with open(f'{cwd_path}"/Raw/data/"{run_id}.txt', 'r') as text:
+        with open(f'{path_drive}"/Raw/data/"{run_id}.txt', 'r') as text:
             texto = text.read()
         if len(texto) < 4:
             lista_artigo_limpo.append(nan)
         else:
             try:
-		if drop_punct:  
-			texto = remove_punctuation(texto)
-		if strip_accents:  # NÃO PODE SER ELIF PQ PRECISA CHECAR TODAS AS CONDIÇÕES!!!
-			texto = strip_accents(texto)
-		if drop_stopwords:
-			texto = remove_stop_words(texto)
-		if stem_and_lem:
-			texto = stem_and_lemmatize(texto)
-		if clean_text:
-                	texto = retorna_texto_limpo(texto)
-		lista_artigo_limpo.append(texto)
+                if drop_punct:
+                    texto = remove_punctuation(texto)
+                if strip_accents:  # NÃO PODE SER ELIF PQ PRECISA CHECAR TODAS AS CONDIÇÕES!!!
+                    texto = strip_accents(texto)
+                if drop_stopwords:
+                    texto = remove_stop_words(texto)
+                if stem_and_lem:
+                    texto = stem_and_lemmatize(texto)
+                if clean_text:
+                    texto = retorna_texto_limpo(texto)
+                lista_artigo_limpo.append(texto)
             except:
                 print('HOUVE UM ERRO DURANTE O PRÉ-PROCESSAMENTO')
 
     df['artigo'] = lista_artigo_limpo
     data_do_dia = str(date.today())
-    df.to_csv(f'{path_drive}/Prepoc/{data_do_dia}.csv')
+    
+    # Checando se arquivo já existe para determinar o run id
+    for i in range(0, 1000):
+        path_file = f'{path_drive}/Prepoc/{data_do_dia}_{i}.csv'
+        if not path.exists(path_file):
+            df.to_csv(f'{path_drive}/Prepoc/{data_do_dia}_{i}.csv')
+            break

dbfcb0a7e2402e05a03f45b2626b91577088c2d4
mittelmax <maxmitteldorf@gmail.com>
2021-03-05 02:07:01 -0300

    arrumei a index col das funcs de teste

--- Vars/var_funcs.py
+++ Vars/var_funcs.py
@@ -1,16 +1,15 @@
 import pandas as pd
 from aux_funcs.set_path import path_drive
 
+
 # Funções de criação de variáveis
-
-
 def teste(run_id):
-    df = pd.read_csv(f"{path_drive}/Prepoc/{run_id}.csv")
+    df = pd.read_csv(f"{path_drive}/Prepoc/{run_id}.csv", index_col=0)
     df["teste"] = "teste de função"
     return df
 
 
 def blabla(run_id):
-    df = pd.read_csv(f"{path_drive}/Prepoc/{run_id}.csv")
+    df = pd.read_csv(f"{path_drive}/Prepoc/{run_id}.csv",index_col=0)
     df["blabla"] = "blabla"
     return df

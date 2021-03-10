7618e16e64a28448a79635b25b425134e78247bc
rodrigogcoelhoo <rodrigogc4@al.insper.edu.br>
2021-03-10 19:41:24 -0300

    TQDM implementado no save_run_preproc

--- aux_funcs/save_run_prepoc.py
+++ aux_funcs/save_run_prepoc.py
@@ -4,14 +4,15 @@ import pandas as pd
 from Preproc.funcs_preproc import *
 from os import path
 from aux_funcs.set_path import path_drive
+from tqdm import tqdm
 
 
 def save_run_preproc(drop_punct=False, strip_accents=False, drop_stopwords=False, stem_and_lem=False, clean_text=True):
     df = pd.read_csv(path_drive + "/Raw/Values/index.csv", index_col=0)
     coluna_run_id = list(df.unique_identifier)
     lista_artigo_limpo = []
-    for run_id in coluna_run_id:
-        with open(f'{path_drive}/Raw/data/{run_id}.txt', 'r') as text:
+    for index_run_id in tqdm(range(len(coluna_run_id))):
+        with open(f'{path_drive}/Raw/data/{coluna_run_id[index_run_id]}.txt', 'r') as text:
             texto = text.read()
         if len(texto) < 4:
             lista_artigo_limpo.append(nan)
@@ -33,13 +34,12 @@ def save_run_preproc(drop_punct=False, strip_accents=False, drop_stopwords=False
 
     df['artigo'] = lista_artigo_limpo
     data_do_dia = str(date.today())
-    
+
     # Checando se arquivo já existe para determinar o run id
     for i in range(0, 1000):
         path_file = f'{path_drive}/Preproc/{data_do_dia}_{i}.csv'
         if not path.exists(path_file):
             df.to_csv(f'{path_drive}/Preproc/{data_do_dia}_{i}.csv')
             break
-
 
 # # # save_run_prepoc()  # # Exemplo -> Sem parâmetros pega clean_text como default = Faz tudo

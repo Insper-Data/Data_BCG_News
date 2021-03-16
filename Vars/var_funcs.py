0b12a02fb6d5c66441391700be2210c8de8f4673
rodrigogcoelhoo <rodrigogc4@al.insper.edu.br>
2021-03-15 21:51:46 -0300

    Limpando e reduzindo dfs + usando o parquet

--- Vars/var_funcs.py
+++ Vars/var_funcs.py
@@ -33,9 +33,19 @@ def var_tfidf(path_drive, min_df, max_features, run_id):
     print("Criando DataFrame denso")
     variaveis = vectorizer.fit_transform(df.artigo)
     df_tfidf = pd.DataFrame(variaveis.todense(), columns=vectorizer.get_feature_names())
+    
+    print("Convertendo variáveis para float32")
+    df_tfidf = df_tfidf.astype('float32')
 
     print("Concatenando DataFrames")
     df_final = pd.concat([df, df_tfidf], axis=1)
+    
+    print("Limpando DF final")
+    df_final.drop(["sigla","nome_jornal","termo_de_busca","data","manchete", "artigo"], axis=1, inplace=True)
+    
+    print("Splitando DF em 10/90")
+    df_final_10 = df_final.sample(frac = 0.1) 
+    df_final_90 = df_final.drop(df_final_10.index)
 
     print("Criação de variáveis por TF-IDF finalizada")
-    return df_final
+    return df_final_10, df_final_90
--- aux_funcs/save_run_variables.py
+++ aux_funcs/save_run_variables.py
@@ -15,7 +15,7 @@ def save_run_variable(func_name, min_df=0.001, max_features=10000, run_id=""):
     data = str(date.today())
 
     # Running var function with eval
-    var = eval(func_name)(path_drive, min_df, max_features, run_id)
+    var_10, var_90 = eval(func_name)(path_drive, min_df, max_features, run_id)
 
     # Pickle with preproc info
     info = {"preproc": run_id,
@@ -24,10 +24,12 @@ def save_run_variable(func_name, min_df=0.001, max_features=10000, run_id=""):
 
     # Checando se arquivo já existe para determinar o run id
     for i in range(0, 1000):
-        path_file = f'{path_drive}/Variables/{func_name}_{data}_{i}.csv'
-        if not path.exists(path_file):
-            var.to_csv(f'{path_drive}/Variables/{func_name}_{data}_{i}.csv')
-            info["variable"] = f'{func_name}_{data}_{i}'
+        path_file_10 = f'{path_drive}/Variables/{func_name}_10_{data}_{i}.csv'
+        path_file_90 = f'{path_drive}/Variables/{func_name}_90_{data}_{i}.csv'
+        if not path.exists(path_file_10) and not path.exists(path_file_90):
+            var_10.to_parquet(f'{path_drive}/Variables/{func_name}_10_{data}_{i}.parquet')
+            var_90.to_parquet(f'{path_drive}/Variables/{func_name}_90_{data}_{i}.parquet')
+            info["variable"] = [f'{func_name}_10_{data}_{i}', f'{func_name}_90_{data}_{i}']
             with open(f'{path_drive}/Variables/info_{func_name}_{data}_{i}.pickle', 'wb') as file:
                 pk.dump(info, file)
             break

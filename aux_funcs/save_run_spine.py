6b04fc1ace39d502241d3f6ea7bdc41efbdb85d9
mittelmax <maxmitteldorf@gmail.com>
2021-03-15 23:54:40 -0300

    adicionei parâmetro da partição

--- aux_funcs/save_run_spine.py
+++ aux_funcs/save_run_spine.py
@@ -5,7 +5,7 @@ import os
 from aux_funcs.set_path import path_drive
 
 
-def save_run_spine(run_id_list):
+def save_run_spine(run_id_list, pct_train=0.1):
 
     # Getting today's date
     data = str(date.today())
@@ -29,12 +29,12 @@ def save_run_spine(run_id_list):
         df_spine = pd.concat(df_list, axis=1).reset_index()
         print("Splitando DF em 10/90")
 
-        df_spine_10 = df_spine.sample(frac=0.1)
+        df_spine_10 = df_spine.sample(frac=pct_train)
         df_spine_90 = df_spine.drop(df_spine_10.index)
     else:
         df_spine = pd.read_csv(paths_vars[0])
         print("Splitando DF em 10/90")
-        df_spine_10 = df_spine.sample(frac=0.1)
+        df_spine_10 = df_spine.sample(frac=pct_train)
         df_spine_90 = df_spine.drop(df_spine_10.index)
 
     # Saving spine info on list of dictionaries
@@ -48,8 +48,8 @@ def save_run_spine(run_id_list):
     for i in range(0, 1000):
         path_file = f'{path_drive}/Spine/spine_{data}_{i}.csv'
         if not os.path.exists(path_file):
-            df_spine_10.to_parquet(f'{path_drive}/Spine/spine_10_{data}_{i}.parq', index=False)
-            df_spine_90.to_parquet(f'{path_drive}/Spine/spine_90_{data}_{i}.parq', index=False)
+            df_spine_10.to_parquet(f'{path_drive}/Spine/spine_{pct_train*100}_{data}_{i}.parq', index=False)
+            df_spine_90.to_parquet(f'{path_drive}/Spine/spine_{100-pct_train*100}_{data}_{i}.parq', index=False)
             with open(f'{path_drive}/Spine/info_spine_{data}_{i}.pickle', 'wb') as file:
                 pickle.dump(spine_info, file)
             break

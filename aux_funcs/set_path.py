83ad85f10d79d47700b5e29aeb58065087204fe1
wilgnerl <wilgnerl@al.insper.edu.br>
2021-05-07 23:33:13 -0300

    Testando e consertando model v2

--- aux_funcs/set_path.py
+++ aux_funcs/set_path.py
@@ -1,2 +1,2 @@
-path_user = {'RODRIGO': "/Users/rodrigo/Insper - Institudo de Ensino e Pesquisa/Max Mitteldorf - Data_BCG_News",
-             'WILGNER': "C:\\Users\\wilgn\\OneDrive - Insper - Institudo de Ensino e Pesquisa\\Data_BCG_News"}
+{'RODRIGO': "/Users/rodrigo/Insper - Institudo de Ensino e Pesquisa/Max Mitteldorf - Data_BCG_News",
+             'WILGNER': r"C:/Users/wilgn/OneDrive - Insper - Institudo de Ensino e Pesquisa/Data_BCG_News/"}
